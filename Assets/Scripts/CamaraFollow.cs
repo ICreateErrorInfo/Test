d4a7ca39f85b589a8da8022774550d44311fcd01
ICreateErrorInfo <hm.github@smjh.de>
2020-02-13 19:48:05 +0100

    Camera Following

---
+++ Assets/Scripts/CamaraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 unterschied;
+
+    private void Start()
+    {
+        unterschied = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + unterschied;
+    }
+}
