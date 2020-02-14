f335a1977781bfb5a5d14d0c6e9900bef853e1e2
IInspectable <github@smjh.de>
2020-02-14 19:05:36 +0100

    ItemEntferner BugFix

--- Assets/Scripts/ItemEntfernen.cs
+++ Assets/Scripts/ItemEntfernen.cs
@@ -2,16 +2,31 @@
 
 using UnityEngine;
 
-public class ItemEntfernen : MonoBehaviour
-{
+public class ItemEntfernen: MonoBehaviour {
+
     public GameObject Obj;
 
+    private bool _insideItem;
+
     [UsedImplicitly]
-    private void OnTriggerStay2D(Collider2D collision) {
-        if (collision.tag == "Player") {
-            if (Input.GetKeyDown(KeyCode.E)) {
-                Destroy(Obj);
-            }
+    private void Update() {
+        if (_insideItem && Input.GetKeyDown(KeyCode.E)) {
+            Destroy(Obj);
         }
     }
+
+    [UsedImplicitly]
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            _insideItem = true;
+        }
+    }
+
+    [UsedImplicitly]
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            _insideItem = false;
+        }
+    }
+
 }
