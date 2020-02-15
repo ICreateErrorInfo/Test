148a23568ee1b82c25740e2120ccd8971a3dfcc6
IInspectable <github@smjh.de>
2020-02-15 16:45:24 +0100

    EText added

--- Assets/Scripts/ItemEntfernen.cs
+++ Assets/Scripts/ItemEntfernen.cs
@@ -1,18 +1,30 @@
 ﻿using JetBrains.Annotations;
-
+using TMPro;
 using UnityEngine;
 
 public class ItemEntfernen: MonoBehaviour {
 
     public GameObject Obj;
-
+    private int _timer;
     private bool _insideItem;
+    public TextMeshProUGUI EText;
 
-    [UsedImplicitly]
+
+        [UsedImplicitly]
     private void Update() {
         if (_insideItem && Input.GetButtonDown(Steuerung.Benutzen)) {
             Destroy(Obj);
+            EText.enabled = false;
             gameObject.SetActive(false);
+        }
+
+        if (_insideItem) {
+            _timer += 1;
+            Debug.Log(_timer);
+        }
+
+        if (_timer >= 100) {
+            EText.text = "Press E";
         }
     }
 
@@ -27,6 +39,8 @@ public class ItemEntfernen: MonoBehaviour {
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.tag == TagNames.Player) {
             _insideItem = false;
+            EText.enabled = false;
+            _timer = 0;
         }
     }
 
